f87bd9b9ecda8eb1d14a53cab1c0bff9eb1e2a26
kkyungyoon <ruddbs803@gmail.com>
2024-07-13 07:36:13 +0900

    programmers-Level1-추억점수

---
+++ Python/0713_1.py
@@ -0,0 +1,20 @@
+def solution(name, yearning, photo):
+    name_list = [i for i in name]
+    yearning_list = [j for j in yearning]
+    name_tuple = tuple(name_list) # dict키값으로 쓰고자 튜플변환
+    
+    # dict 생성
+    dict={}
+    for i in range(len(name_list)):
+        dict[name_list[i]] = yearning_list[i]
+    
+    answer = []
+    for pho in photo:
+        sum = 0
+        for person in pho:
+            if person in name_list:
+                sum += dict[person]
+            else:
+                continue
+        answer.append(sum)
+    return answer
