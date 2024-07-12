e3033757606999554370ca46dda6dad2f69667c5
kkyungyoon <ruddbs803@gmail.com>
2024-07-13 07:41:45 +0900

    programmers-Level1-추억점수

--- Python/0713_1.py
+++ Python/0713_1.py
@@ -1,13 +1,16 @@
 def solution(name, yearning, photo):
-    name_list = [i for i in name]
-    yearning_list = [j for j in yearning]
-    name_tuple = tuple(name_list) # dict키값으로 쓰고자 튜플변환
+    # name_list = [i for i in name]
+    # yearning_list = [j for j in yearning]
+    # name_tuple = tuple(name_list) # dict키값으로 쓰고자 튜플변환
     
     # dict 생성
-    dict={}
-    for i in range(len(name_list)):
-        dict[name_list[i]] = yearning_list[i]
-    
+    # dict={}
+    # for i in range(len(name_list)):
+    #     dict[name_list[i]] = yearning_list[i]
+
+    # dict 생성 (zip으로 한번에 해결)
+    dictionary = dict(zip(name,yearning))
+
     answer = []
     for pho in photo:
         sum = 0
