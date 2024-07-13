f7ee53e8342144be9137540ad02543848750a73f
kkyungyoon <ruddbs803@gmail.com>
2024-07-13 14:13:50 +0900

    programmers-Level0-부분문자열인지 확인하기

---
+++ Python/0713_2.py
@@ -0,0 +1,11 @@
+def solution(my_string, target):
+    if target in my_string:
+        return 1
+    else:
+        return 0
+
+def solution(my_string, target):
+    return 1 if target in my_string else 0
+
+def solution(my_string, target):
+    return int(target in my_string)
